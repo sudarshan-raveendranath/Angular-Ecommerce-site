3d8935b063c2a3e142bc993a918dec36d5488399
sudarshan raveendranath dayarathna <120754357+sudarshan-raveendranath@users.noreply.github.com>
2024-03-09 20:06:36 +0530

    Implementing changes to the Angular application:
    - Modified app.component.html, app.component.ts, and app.module.ts.
    - Added a new file header.component.ts in the header folder.
    - Modified index.html.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
