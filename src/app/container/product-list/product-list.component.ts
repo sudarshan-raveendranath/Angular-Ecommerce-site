1e88e3dace74f1b6e2f4c78ebdfda2e585b939ec
sudarshan raveendranath dayarathna <120754357+sudarshan-raveendranath@users.noreply.github.com>
2024-06-26 19:27:47 +0530

    modified:   src/app/container/product-list/filter/filter.component.html 	modified:   src/app/container/product-list/filter/filter.component.ts 	modified:   src/app/container/product-list/product-list.component.html 	modified:   src/app/container/product-list/product-list.component.ts

--- src/app/container/product-list/product-list.component.ts
+++ src/app/container/product-list/product-list.component.ts
@@ -537,4 +537,11 @@ export class ProductListComponent {
   totalProductInStock = this.products.filter((p) => p.is_in_inventory == true).length;
   totalProductOutOfStock = this.products.filter((p) => p.is_in_inventory == false).length;
 
+  selectedFilterRadioButton: string = "all";
+
+  onFilterChanged(value: string) {
+    console.log(value);
+    this.selectedFilterRadioButton = value;
+  }
+
 }
